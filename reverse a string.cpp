53b17dc81f0cde931e68d91ca1dabefc5272876f
deshmukh-faiz <126980027+deshmukh-faiz@users.noreply.github.com>
2023-06-27 13:14:53 +0530

    Add files via upload

---
+++ reverse a string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string text1 = "hey dosto";
+    reverse(text1.begin(), text1.end());
+
+    cout << text1 << endl;
+}
